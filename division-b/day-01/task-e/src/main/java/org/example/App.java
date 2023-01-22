d1fad6f3b39c92605db24559f97e76c1298eda01
19740782 <APaPchelina@sberbank.ru>
2023-01-22 16:32:47 +0300

    task e

--- division-b/day-01/task-e/src/main/java/org/example/App.java
+++ division-b/day-01/task-e/src/main/java/org/example/App.java
@@ -1,13 +1,45 @@
 package org.example;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Падает на втором тесте
+
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+
+        int d = Integer.parseInt(br.readLine());
+        String[] strings = br.readLine().split(" ");
+        int[] point = new int[strings.length];
+        System.out.println(App.getResult(d, point[0], point[1]));
+    }
+
+    public static int getResult(int d, int xo, int yo) {
+
+        // найти все случаи, когда точка лежит за пределами квадрата
+        // если вошли внутрь, посчитать длины отрезков
+        if (d >= 0 && (yo < 0 || xo < 0 || yo > d - xo) ||
+                (d <= 0 && (yo > 0 || xo > 0 || yo < d - xo))) {
+
+            int xa = 0, ya = 0;
+            int xb = d, yb = 0;
+            int xc = 0, yc = d;
+
+            double lenA = getSegmentLength(xa, ya, xo, yo);
+            double lenB = getSegmentLength(xb, yb, xo, yo);
+            double lenC = getSegmentLength(xc, yc, xo, yo);
+
+            if (lenA <= lenB && lenA <= lenC) return 1;
+            else if (lenB <= lenC) return 2;
+            return 3;
+        }
+        return 0;
+    }
+
+    private static double getSegmentLength(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
     }
 }
--- division-b/day-01/task-e/src/test/java/org/example/AppTest.java
+++ division-b/day-01/task-e/src/test/java/org/example/AppTest.java
@@ -1,20 +1,73 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
+import java.util.stream.Stream;
 
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+import static org.junit.Assert.assertEquals;
+
+class AppTest {
+
+    private static Stream<Arguments> source() {
+        return Stream.of(
+                Arguments.of(5, 1, 1, 0),
+                Arguments.of(3, -1, -1, 1),
+                Arguments.of(4, 4, 4, 2),
+                Arguments.of(4, 2, 2, 0),
+                Arguments.of(5, 0, 0, 0),
+                Arguments.of(5, -1, -1, 1),
+                Arguments.of(5, 4, 0, 0),
+                Arguments.of(5, 0, 0, 0),
+                Arguments.of(0, 0, 0, 0),
+                Arguments.of(-5, 1, 1, 1),
+                Arguments.of(-5, -1, -1, 0),
+                Arguments.of(0, 1, 1, 1),
+                Arguments.of(0, -1, 1, 1),
+                Arguments.of(5, 0, 0, 0),
+                Arguments.of(5, 2, 0, 0),
+                Arguments.of(5, 5, 0, 0),
+                Arguments.of(5, 3, 2, 0),
+                Arguments.of(5, 0, 5, 0),
+                Arguments.of(5, 0, 2, 0),
+                Arguments.of(5, 7, 0, 2),
+                Arguments.of(5, 5, 2, 2),
+                Arguments.of(5, 0, 6, 3),
+                Arguments.of(5, -1, 5, 3),
+                Arguments.of(5, -1, 2, 1),
+                Arguments.of(5, -1, 0, 1),
+                Arguments.of(5, -1, -1, 1),
+                Arguments.of(5, 0, -1, 1),
+                Arguments.of(5, 1, -1, 1),
+                Arguments.of(-5, 0, 0, 0),
+                Arguments.of(-5, 0, -2, 0),
+                Arguments.of(-5, 0, -5, 0),
+                Arguments.of(-5, -2, -3, 0),
+                Arguments.of(-5, -5, 0, 0),
+                Arguments.of(-5, -2, 0, 0),
+                Arguments.of(-5, -1, -1, 0),
+                Arguments.of(-5, -1, 1, 1),
+                Arguments.of(-5, 0, 1, 1),
+                Arguments.of(-5, 1, 1, 1),
+                Arguments.of(-5, 1, -1, 1),
+                Arguments.of(-5, 1, -4, 3),
+                Arguments.of(-5, 1, -5, 3),
+                Arguments.of(-5, 1, -6, 3),
+                Arguments.of(-5, 0, -6, 3),
+                Arguments.of(-5, -1, -6, 3),
+                Arguments.of(-5, -1, -5, 3),
+                Arguments.of(-5, -5, -1, 2),
+                Arguments.of(-5, -6, -1, 2),
+                Arguments.of(-5, -6, 0, 2),
+                Arguments.of(-5, -6, 1, 2),
+                Arguments.of(-5, -5, 1, 2)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("source")
+    public void shouldCallGetCardByIddMethod(int d, int x1, int x2, int expected) {
+        assertEquals(expected, App.getResult(d, x1, x2));
     }
 }
